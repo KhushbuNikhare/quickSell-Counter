1bb1f609a1863a65c5d0dd10687cf08144dda70e
KhushbuNikhare <khushbunikhare@gmail.com>
2022-04-26 16:07:50 +0530

    done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,27 +2,20 @@ import React, { useState } from "react";
 import "./Counter.css";
 
 const Counter = () => {
-  const [num, setNum] = useState();
-  const [maxVal, setMaxVal] = useState();
+  const [num, setNum] = useState("");
+  const [maxVal, setMaxVal] = useState("");
 
   const decNum = () => {
-    if (num) {
-      setNum(+num - 1);
-    } else {
-      setNum(1 - 1);
-    }
+    if (num > 0) setNum(num - 1);
   };
 
   const incNum = () => {
-    if (num) {
-      setNum(+num + 1);
-    } else {
-      setNum(1 + 1);
+    if (maxVal) {
+      if (+num < maxVal) setNum(+num + 1);
     }
-  };
-
-  const inputNum = (e) => {
-    setNum(e.target.value);
+    if (!maxVal) {
+      if (+num < 25) setNum(+num + 1);
+    }
   };
 
   return (
@@ -32,11 +25,24 @@ const Counter = () => {
           -
         </button>
 
-        <input type="text" value={num} onChange={inputNum} />
+        <input
+          type="text"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+        />
 
         <button className="r-btn" onClick={incNum}>
           +
         </button>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Max Value"
+          value={maxVal}
+          onChange={(e) => setMaxVal(e.target.value)}
+        />
       </div>
     </>
   );
